0a1a6f6c523a21c97002e25cc99f3160c0296df8
Li Ke <72687314+LiKe-rm@users.noreply.github.com>
2023-10-20 16:55:13 +0800

    Update AutoTencent.py

--- AutoTencent.py
+++ AutoTencent.py
@@ -2,6 +2,8 @@
 from selenium import webdriver
 from selenium.webdriver.edge.options import Options
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import time,datetime
 
 # start=time.time()

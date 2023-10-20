fd7f54152dcb632049f455606bf679b65973a852
Li Ke <72687314+LiKe-rm@users.noreply.github.com>
2023-10-20 16:51:10 +0800

    Update AutoTencent.py
    1.考虑网络时延导致元素加载不及时，进而无法定位元素的情况，使用WebDriverWait(driver, timeout).until机制解决该问题；
    2.修改等待执行时间的机制，取消了循环和sleep(1)的低效实现。
    总结：脚本效率基本提速到上限，填写速度仅受限于网络时延。

--- AutoTencent.py
+++ AutoTencent.py
@@ -33,21 +33,34 @@ while True:    # 等待通过手机扫码或者其他方式登录，之后输入
 # print("快捷登录成功")
 # time.sleep(1)
 
-# 设定执行时间2023年9月20日18点
-execute_time = datetime.datetime(2023, 9, 25, 18, 0, 0)
+# 设定执行时间2023年9月20日14点
+execute_time = datetime.datetime(2023, 10, 20, 16, 38, 0)
+
+# 计算需要等待的时间
+wait_time = execute_time - datetime.datetime.now()
 
 # 等待到指定时间再执行
-print("Waiting for the begining...")
-while datetime.datetime.now() < execute_time:
-    time.sleep(1)
-print("Begining!")
+if wait_time.total_seconds() > 0:
+    print("Waiting for the beginning...")
+    time.sleep(wait_time.total_seconds())
+print("Beginning!")
 start = time.time()
-driver.execute_script("window.location.reload()")   # 刷新网页
-elmet = driver.find_elements(By.XPATH,"//textarea[@placeholder='请输入']")
-elmet[0].send_keys("11")   # 输入需要填写的信息：姓名、联系方式...
-elmet[1].send_keys("22")
-elmet[2].send_keys("33")
-elmet[3].send_keys("44")
+# 刷新网页
+driver.execute_script("window.location.reload()")
+
+# 等待元素出现
+timeout = 10  # 设置超时时间，单位为秒
+locator = (By.XPATH, "//textarea[@placeholder='请输入']")
+elements = []
+while not elements:
+    try:
+        elements = WebDriverWait(driver, timeout).until(EC.presence_of_all_elements_located(locator))
+    except:
+        pass
+elements[0].send_keys("11")   # 输入需要填写的信息：姓名、联系方式...
+elements[1].send_keys("22")
+elements[2].send_keys("33")
+elements[3].send_keys("44")
 
 # 由于不同的网页收集表提交和确认按钮似乎会变化，不推荐使用css_selector选择
 # button = driver.find_element(By.CSS_SELECTOR,'#root > div.form-root.fill-form-root > div > div > div.form-fill-container > div.form-with-history-record.fill-area > div.form-body.form-fill-body > div.question-commit > button')
@@ -61,8 +74,8 @@ elmet[3].send_keys("44")
 
 button = driver.find_element(By.XPATH,"//button[text()='提交']")
 driver.execute_script("arguments[0].click();", button)
-time.sleep(1)
-button = driver.find_element(By.XPATH,"//button[contains(.,'确认')]")
+locator = (By.XPATH, "//button[contains(.,'确认')]")
+button = WebDriverWait(driver, timeout).until(EC.presence_of_element_located(locator))
 button.click()
 
 print(time.time()-start)
